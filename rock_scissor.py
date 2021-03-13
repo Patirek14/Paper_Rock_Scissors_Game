7247134a27adbadd203720d5d0534ec712adc299
Patirek14 <79324581+Patirek14@users.noreply.github.com>
2021-03-13 11:54:31 +0100

    Optimalization and support for config file

--- rock_scissor.py
+++ rock_scissor.py
@@ -1,56 +1,68 @@
 import time,random
+
+key_codes = {"paper":"P", "rock":"R", "scissors":"S"}
+map_codes = ["paper","rock","scissors"]
+init_time = 3
+
+def config_game():
+    file = open("config.config")
+    line = file.read(1)
+    init_time = int(line)
+    line = file.read(1)
+    key_codes["paper"] = line
+    line = file.read(1)
+    key_codes["rock"] = line
+    line = file.read(1)
+    key_codes["scissors"] = line
+    #print(key_codes)
+    #print(init_time)
+def is_key_right(k):
+    return k in key_codes.values()
 def clear():
     for x in range(300):
         print("")
-
+        
+comPoints = 0
+playerPoints = 0
+config_game()
 print("==================HELLO==================")
 print("This is a rock paper scissors game , when you ready press any key to continue.")
 print("==================HOW TO PLAY==================")
-print("To paper press P on keyboard for rock press R for scissors press S")
+print("To paper press "+ key_codes["paper"] + " on keyboard for rock press "+ key_codes["rock"] + " for scissors press "+ key_codes["scissors"])
 input()
-#clear()
-print("3..")
-#clear()
-time.sleep(1)
-print("2..")
-#clear()
-time.sleep(1)
-print("1..")
-time.sleep(1)
+for x in range(init_time,0,-1):
+    print(str(x) + "...")
+    time.sleep(1)
 print("START")
-comPoints = 0
-playerPoints = 0
 while(comPoints < 10 and playerPoints < 10):
     x = input("Your turn !!!: ")
+    if(not is_key_right(x)):
+        print("Wrong key")
+        continue
     c = random.randrange(3)
-    if c == 0:
-        print("Computer choosed paper")
-    if c == 1:
-        print("Computer choosed rock")
-    if c == 2:
-        print("Computer choosed scissor")
-    if c == 0 and x == "P":
+    print("Computer choosed " + map_codes[c])
+    if c == 0 and x == key_codes["paper"]:
         comPoints = comPoints + 1
         playerPoints = playerPoints+ 1
-    elif c == 0 and x == "R":
+    elif c == 0 and x == key_codes["rock"]:
         comPoints =+ 1
-    elif c == 0 and x == "S":
+    elif c == 0 and x == key_codes["scissors"]:
         playerPoints = playerPoints + 1
         ###############
-    elif c == 1 and x == "R":
+    elif c == 1 and x == key_codes["rock"]:
         comPoints = comPoints + 1
         playerPoints = playerPoints + 1
     elif c == 1 and x == "S":
         comPoints = comPoints + 1
-    elif c == 1 and x == "P":
+    elif c == 1 and x == key_codes["paper"]:
         playerPoints = playerPoints + 1
         ###############
-    elif c == 2 and x == "S":
+    elif c == 2 and x == key_codes["scissors"]:
         comPoints =  comPoints + 1
         playerPoints = playerPoints + 1
-    elif c == 2 and x == "P":
+    elif c == 2 and x == key_codes["paper"]:
         comPoints =  comPoints + 1
-    elif c == 2 and x == "R":
+    elif c == 2 and x == key_codes["rock"]:
         playerPoints = playerPoints + 1
     print(str(playerPoints) + " : " + str(comPoints))
 
@@ -60,6 +72,3 @@ elif playerPoints == comPoints:
     print("REMIS")
 else:
     print("COMPUTER HAS WON")
-
-  #MADE BY PATIREK 14  
-    
